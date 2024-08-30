a680dc1505bf1cba4bed94d2b10ca1ba13d8b8d0
Михаил Дорошин <demargorn@gmail.com>
2024-08-30 16:31:43 +0300

    Classes.js added

--- src/Classes.js
+++ src/Classes.js
@@ -1,35 +1,4 @@
-class Character {
-   constructor(name) {
-      this.name = name;
-      this.health = 100;
-      this.level = 1;
-      this.attack = null;
-      this.defence = null;
-
-      if (this.name.length < 2 || this.name.length > 10) {
-         throw new Error(`Your name must be at least 2 and no more than 10 characters`);
-      }
-   }
-
-   levelUp() {
-      if (this.health === 0) {
-         throw new Error(`You cannot increase the level of the deceased hero`);
-      }
-
-      this.level += 1;
-      this.health = 100;
-      this.attack += Math.floor(this.attack * 0.2);
-      this.defence += Math.floor(this.defence * 0.2);
-   }
-
-   damage(points) {
-      if (this.health <= 0) {
-         throw new Error(`You are dead`);
-      }
-
-      this.health -= Math.floor(points * (1 - this.defence / 100));
-   }
-}
+import { Character } from './Character';
 
 class Bowman extends Character {
    constructor(name) {
